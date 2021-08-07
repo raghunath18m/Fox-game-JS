59a350bacf5bd10cbb84477dfb19dc395dfdb2e5
raghunath18m <raghunath18m@gmail.com>
2021-08-07 19:00:25 +0530

    feat: initialized buttons and styled them
    initialized buttons and styled them

--- src/init.js
+++ src/init.js
@@ -1,9 +1,10 @@
-import game from "./gameState";
-
-const TICK_RATE = 3000;
+import game, { handleUserAction } from "./gameState";
+import initButtons from "./button";
+import { TICK_RATE } from "./constants";
 
 async function init() {
   console.log("starting game");
+  initButtons(handleUserAction);
 
   let nextTimeToTick = Date.now();
   function nextAnimationFrame() {
